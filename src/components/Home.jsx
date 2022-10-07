37a2b9a658e7ca7de5dfd9a23c69e359b449f075
ingramirezrivera <ing.ramirezrivera@gmail.com>
2022-10-07 10:44:40 -0500

    Pricing Section ready

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,13 +2,16 @@ import React from 'react';
 import Navbar from './Navbar';
 import Hero from './Hero';
 import './HomeStyles.css';
+import Pricing from './Pricing';
+import Testimonials from './Testimonials';
 
 const Home = () => {
   return (
     <div>
         <Navbar />
         <Hero />
-      
+        <Pricing />
+        <Testimonials />
     </div>
   )
 }
