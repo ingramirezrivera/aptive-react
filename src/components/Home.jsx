2cb8e6a2e355b8f8bd303893cf6c6f9d767f1bf4
ingramirezrivera <ing.ramirezrivera@gmail.com>
2022-10-07 08:48:07 -0500

    My first commit

---
+++ src/components/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './HomeStyles.css';
+import Navbar from './Navbar';
+
+const Home = () => {
+  return (
+    <div>
+        <Navbar />
+      
+    </div>
+  )
+}
+
+export default Home;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Link } from "react-router-dom";
+import { FaBars, FaTimes } from 'react-icons/fa';
+import Logo from '../assets/logo.png';
+
+
+const Navbar = () => {
+  return (
+    <header>
+        <nav className='navbar'>
+            <div className='logo'>
+                <Link to='/'><img src={Logo} alt='Logo' /></Link>
+            </div>
+            <ul className='nav-menu'>
+                <li className='nav-item'>
+                    <Link to='/' className='nav-link'>Home</Link>
+                </li>
+                <li className='nav-item'>
+                    <Link to='/' className='nav-link'>Pricing</Link>
+                </li>
+                <li className='nav-item'>
+                    <Link to='/' className='nav-link'>FAQ</Link>
+                </li>
+                <li className='nav-item'>
+                    <Link to='/' className='nav-link'>Contact</Link>
+                </li>
+            </ul>
+            <div className='hamburguer'>
+                <FaBars />
+            </div>
+        </nav>
+      
+    </header>
+  )
+};
+
+export default Navbar
